f6175bf8a958585d1996648ea09e61d9e10fb67c
Giulio Venturini <86805736+BayoDev@users.noreply.github.com>
2025-09-22 14:26:41 +0200

    Added sitemap generation

--- update.sh
+++ update.sh
@@ -12,3 +12,19 @@ for file in md_posts/*.md; do
 			-B components/header.html \
 			-A components/footer.html
 done
+
+BASE_URL="https://bayo.dev"
+
+SITEMAP="sitemap.xml"
+
+echo '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' > "$SITEMAP"
+
+echo "  <url><loc>${BASE_URL}/</loc></url>" >> "$SITEMAP"
+
+# Add URLs for each HTML file in html_posts
+for file in html_posts/*.html; do
+    filename=$(basename "$file")
+    echo "  <url><loc>${BASE_URL}/html_posts/${filename}</loc></url>" >> "$SITEMAP"
+done
+
+echo '</urlset>' >> "$SITEMAP"
